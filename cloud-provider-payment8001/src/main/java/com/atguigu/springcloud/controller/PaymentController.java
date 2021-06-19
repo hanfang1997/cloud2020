4fc48a7a8ac1b4b0711b1c79865522a96229c6a9
韩放 <1767495738@qq.com>
2021-06-19 15:07:14 +0800

    sleuth+zipkin 链路追踪

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -84,4 +84,9 @@ public class PaymentController {
         }
         return serverPort;
     }
+
+    @GetMapping("/payment/zipkin")
+    public String paymentZipkin(){
+        return "hi,i am payment zipkin server fall back, welcome to atguigu, O(∩_∩)O哈哈~";
+    }
 }
